de73bd678eb202d192bac95e5a34877dd7ca84ef
Jaehyun <vgb30xk@naver.com>
2023-04-12 11:48:52 +0900

    4/12일 공부

--- rpa_basic/2_desktop/8_keyboard.py
+++ rpa_basic/2_desktop/8_keyboard.py
@@ -1,3 +1,4 @@
+import pyperclip
 import pyautogui
 # for w in pyautogui.getAllWindows():
 #     print(w)
@@ -11,3 +12,22 @@ w.activate()
 
 # pyautogui.write(["t", "e", "s", "t","left","left","right","l","a","enter"], interval=0.25)
 
+# 조합키 (핫키)
+# pyautogui.keyDown("ctrl")
+# pyautogui.keyDown("a")
+# pyautogui.keyUp("a")
+# pyautogui.keyUp("ctrl")
+
+# 간편한 조합키
+# pyautogui.hotkey("ctrl",  "a")
+
+# pyperclip.copy("재현코딩")
+# pyautogui.hotkey("ctrl", "v")
+
+
+def my_write(text):
+    pyperclip.copy(text)
+    pyautogui.hotkey("ctrl", "v")
+
+
+my_write("마마 코딩")
---
+++ rpa_basic/2_desktop/9_message_box.py
@@ -0,0 +1,5 @@
+import pyautogui
+# print("곧 시작합니다...")
+# pyautogui.countdown(3)
+# print("자동화 시작")
+
